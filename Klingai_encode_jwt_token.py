9abc0f4a5c6b6d95d9f577e7b915f092234c49b0
1367258619@qq.com <1367258619@qq.com>
2025-03-01 13:47:06 +0800

    update

--- Klingai_encode_jwt_token.py
+++ Klingai_encode_jwt_token.py
@@ -25,7 +25,7 @@ def encode_jwt_token():
     }
     payload = {
         "iss": ak,
-        "exp": int(time.time()) + 1800,
+        "exp": int(time.time()) + 4800,
         "nbf": int(time.time()) - 5 
     }
     token = jwt.encode(payload, sk, headers=headers)
